a36b581521c0a61cad0c905a8850f5193b5c8312
ShadyEgypt <shadyrafat60@gmail.com>
2023-10-01 15:23:14 +0300

    register the blueprint

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,13 +4,19 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-
+import os
 app = Flask(__name__)
+
+# Register the app_views blueprint:
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Calls storage.close() at the end of the request"""
+    """
+    Calls storage.close() at the end of the request
+    """
     storage.close()
 
 
