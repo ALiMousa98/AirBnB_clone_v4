0dfc490c64153ee362e139c0929f1e3c472b058b
ShadyEgypt <shadyrafat60@gmail.com>
2023-10-03 02:27:37 +0300

    task 14

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import hashlib
 
+
 class User(BaseModel, Base):
     """Representation of a user """
     if models.storage_t == 'db':
