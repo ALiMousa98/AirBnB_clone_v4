15475c3cbe3a35b00c2382ddc1990653692c2756
ShadyEgypt <shadyrafat60@gmail.com>
2023-10-03 01:16:40 +0300

    task 13.3

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -31,7 +31,7 @@ def amenities_per_place(place_id=None):
 
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
-                 methods=['DELETE', 'POST'])
+                 methods=['DELETE'])
 def amenity_to_place(place_id=None, amenity_id=None):
     """
         reviews route to handle http methods for given review by ID
@@ -43,28 +43,41 @@ def amenity_to_place(place_id=None, amenity_id=None):
     if amenity is None:
         abort(404, 'Not found')
 
-    if request.method == 'DELETE':
-        if (amenity not in place.amenities or
-                amenity.id not in place.amenity_ids):
-            abort(404, 'Not found')
-        if STORAGE_TYPE == 'db':
-            place.amenities.remove(amenity)
-        else:
-            place.amenity_ids.pop(amenity.id, None)
-        place.save()
-        return jsonify({}), 200
+    if (amenity not in place.amenities or
+            amenity.id not in place.amenity_ids):
+        abort(404, 'Not found')
+    if STORAGE_TYPE == 'db':
+        place.amenities.remove(amenity)
+    else:
+        place.amenity_ids.pop(amenity.id, None)
+    place.save()
+    return jsonify({}), 200
 
-    if request.method == 'POST':
-        if STORAGE_TYPE == 'db':
-            if (amenity in place.amenities):
-                return make_response(jsonify(amenity.to_dict()), 200)
-            else:
-                place.amenities.append(amenity)
-        else:
-            if (amenity.id in place.amenity_ids):
-                return make_response(jsonify(amenity.to_dict()), 200)
-            else:
-                place.amenity_ids.append(amenity_id)
 
-        storage.save()
-        return make_response(jsonify(amenity.to_dict()), 201)
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['POST'])
+def amenity_to_place(place_id=None, amenity_id=None):
+    """
+        reviews route to handle http methods for given review by ID
+    """
+    place = storage.get(Place, place_id)
+    amenity = storage.get(Amenity, amenity_id)
+    if place is None:
+        abort(404, 'Not found')
+    if amenity is None:
+        abort(404, 'Not found')
+
+    if STORAGE_TYPE == 'db':
+        if (amenity in place.amenities):
+            return make_response(jsonify(amenity.to_dict()), 200)
+        else:
+            place.amenities.append(amenity)
+    else:
+        if (amenity.id in place.amenity_ids):
+            return make_response(jsonify(amenity.to_dict()), 200)
+        else:
+            place.amenity_ids.append(amenity_id)
+
+    storage.save()
+    return make_response(jsonify(amenity.to_dict()), 201)
