59451da38f27e4d1b1e2668003ccb520c524dfaa
ShadyEgypt <shadyrafat60@gmail.com>
2023-10-02 22:12:44 +0300

    fix code style

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,12 +5,14 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route("/status", methods=["GET"])
 def status():
     '''
     Returns a JSON response for RESTful API health.
     '''
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats', methods=['GET'])
 def get_stats():
@@ -28,4 +30,3 @@ def get_stats():
         count = storage.count(cls)
         total[classes.get(cls)] = count
     return jsonify(total)
-    
