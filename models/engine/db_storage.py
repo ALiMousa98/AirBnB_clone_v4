58df6176fd098d6c8694e6a882cc08c660946963
ShadyEgypt <shadyrafat60@gmail.com>
2023-10-02 22:01:26 +0300

    improve functions

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,16 +3,14 @@
 Contains the class DBStorage
 """
 
-import models
 from models.amenity import Amenity
-from models.base_model import BaseModel, Base
+from models.base_model import Base
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
 from os import getenv
-import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
@@ -79,13 +77,13 @@ class DBStorage:
         """
         Retrieve an object by class and ID
         """
-        if cls in classes.values() and id and type(id) == str:
+        if cls in classes.values() and id and type(id) is str:
             d_obj = self.all(cls)
             for key, value in d_obj.items():
                 if key.split('.')[1] == id:
                     return value
-        return None 
-    
+        return None
+
     def count(self, cls=None):
         """
         Count the number of obj in storage
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ Contains the FileStorage class
 """
 
 import json
+import models
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
@@ -75,12 +76,12 @@ class FileStorage:
         """
         if cls not in classes.values():
             return None
-        
+
         all_cls = models.storage.all(cls)
         for value in all_cls.values():
             if (value.id == id):
                 return value
-    
+
     def count(self, cls=None):
         """
         Count the number of obj in storage
@@ -92,5 +93,5 @@ class FileStorage:
                 count += len(models.storage.all(i).values())
         else:
             count = len(models.storage.all(cls).values())
-        
+
         return count
