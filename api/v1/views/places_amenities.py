1eae892974fe7ee8abbd0a2a45e768897903d86f
ShadyEgypt <shadyrafat60@gmail.com>
2023-10-03 00:58:31 +0300

    task 13.1

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+Flask route that returns json status response
+"""
+from models import storage
+from models.place import Place
+from models.amenity import Amenity
+from api.v1.views import app_views
+from flask import abort, jsonify, request, make_response
+from flasgger.utils import swag_from
+from os import environ
+STORAGE_TYPE = environ.get('HBNB_TYPE_STORAGE')
+
+
+@app_views.route('/places/<place_id>/amenities', methods=['GET'])
+def amenities_per_place(place_id=None):
+    """
+        reviews route to handle http method for requested reviews by place
+    """
+    place = storage.get(Place, place_id)
+
+    if request.method == 'GET':
+        if place is None:
+            abort(404, 'Not found')
+
+        if STORAGE_TYPE == 'db':
+            amenities = [amenity.to_dict() for amenity in place.amenities]
+        else:
+            amenities = [storage.get(Amenity, amenity_id).to_dict()
+                         for amenity_id in place.amenitiy_id]
+        return jsonify(amenities)
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['DELETE', 'POST'])
+def amenity_to_place(place_id=None, amenity_id=None):
+    """
+        reviews route to handle http methods for given review by ID
+    """
+    place = storage.get(Place, place_id)
+    amenity = storage.get(Amenity, amenity_id)
+    if place is None:
+        abort(404, 'Not found')
+    if amenity is None:
+        abort(404, 'Not found')
+
+    if request.method == 'DELETE':
+        if (amenity not in place.amenities or
+                amenity.id not in place.amenity_ids):
+            abort(404, 'Not found')
+        if STORAGE_TYPE == 'db':
+            place.amenities.remove(amenity)
+        else:
+            place.amenity_ids.pop(amenity.id, None)
+        place.save()
+        return jsonify({}), 200
+
+    if request.method == 'POST':
+        if (amenity in place.amenities or
+                amenity.id in place.amenity_ids):
+            return make_response(jsonify(amenity.to_dict()), 200)
+            
+        if STORAGE_TYPE == 'db':
+            place.amenities.append(amenity)
+        else:
+            place.amenities = amenity
+        return make_response(jsonify(amenity.to_dict()), 201)
